5aae59645be0843539386bd424e443cbbb443e40
takao_tokuno <tokuno.takao.petacreate@gmail.com>
2025-09-16 11:41:16 +0900

    chore: Pomodoro New Tab 拡張の最小構成を追加

---
+++ background.js
@@ -0,0 +1,7 @@
+class PomodoroBackground {
+    constructor() {
+
+    }
+}
+
+new PomodoroBackground();
---
+++ content.js
@@ -0,0 +1,7 @@
+class PomodoroContent {
+    constructor() {
+
+    }
+}
+
+new PomodoroContent();
