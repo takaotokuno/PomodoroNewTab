7a2cb630fe17b1aa8de150d20c9e9ccff774ff41
takao_tokuno <tokuno.takao.petacreate@gmail.com>
2025-09-30 19:47:13 +0900

    コミットメッセージルールが適用されるかテスト

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [2, "never", ["sentence-case"]], // 先頭大文字禁止
+    "header-max-length": [2, "always", 72],          // 72文字制限
+    "type-enum": [2, "always", [
+      "feat", "fix", "docs", "style", "refactor", "test", "chore",
+      "wip",     // 独自追加：作業途中
+      "release"  // 独自追加：リリース用
+    ]],
+  }
+};
