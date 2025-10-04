8e122ba54eb1ad1af406c5d2026776cb04003244
takao_tokuno <tokuno.takao.petacreate@gmail.com>
2025-10-04 17:17:32 +0900

    fix: notification.js: urlをchrome.runtime.getURLで取得するよう変更、コンソールログにも通知するよう追加

--- src/background/notification.js
+++ src/background/notification.js
@@ -5,10 +5,14 @@
  * @param {string} param0.title - Notification title
  * @param {string} param0.message - Notification body
  */
+
+const iconUrl = chrome.runtime.getURL("resources/icon.png");
+
 export async function notify({ id, title, message }) {
+  console.log("Showing notification:", title, message);
   return chrome.notifications.create(id, {
     type: "basic",
-    iconUrl: "icons/icon128.png",
+    iconUrl,
     title,
     message,
     priority: 2,
