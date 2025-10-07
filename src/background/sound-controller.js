9fe92bf7893ad694c8b6a8ade204dc128215d6e5
takao_tokuno <tokuno.takao.petacreate@gmail.com>
2025-10-07 20:54:38 +0900

    add: sound-controller.jsモジュールを追加（未実装

---
+++ src/background/sound-controller.js
@@ -0,0 +1,26 @@
+import { getTimer } from "./timer-store.js";
+import Constants from "../constants.js";
+const { TIMER_MODES, SESSION_TYPES } = Constants
+
+let isPlaying = false;
+
+export async function handleSound() {
+  let timer = getTimer();
+  
+  if(
+    !timer.soundEnabled
+    || timer.mode !== TIMER_MODES.RUNNING
+    || timer.sessionType !== SESSION_TYPES.WORK
+  ) {
+    stopAudio();
+    return;
+  }
+
+  if(isPlaying) return;
+
+  playAudio();
+}
+
+function stopAudio(){
+
+}
