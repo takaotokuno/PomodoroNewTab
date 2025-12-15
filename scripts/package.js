61e8b3d62819577c6476fa01e8992d41deec797b
takao_tokuno <t.tokunou623.halfway@gmail.com>
2025-12-15 23:37:35 +0900

    fix: typoを修正

--- scripts/package.js
+++ scripts/package.js
@@ -1,17 +1,17 @@
 import fs from "fs/promises";
 import { createZip } from "./config/zip-utils.js";
 
-async function pachageExtension() {
+async function packageExtension() {
   const inputDir = "dist/latest";
   const outputDir = "dist/packages";
   try {
     await fs.access(inputDir);
     await fs.mkdir(outputDir, { recursive: true });
-    
+
     const packageJson = JSON.parse(await fs.readFile("package.json", "utf-8"));
     const name = packageJson.name;
     const version = packageJson.version;
-    
+
     const zipFilePath = `${outputDir}/${name}-v${version}.zip`;
 
     await createZip(inputDir, zipFilePath);
@@ -21,4 +21,4 @@ async function pachageExtension() {
   }
 }
 
-pachageExtension();
+packageExtension();
